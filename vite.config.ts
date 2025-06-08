0909639de1f2736a700683531dc7260f9a96c7f8
unanmed <1319491857@qq.com>
2025-06-08 17:59:47 +0800

    fix: jsx 报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import * as glob from 'glob';
 import path from 'path';
+import jsx from '@vitejs/plugin-vue-jsx';
 
 const aliases = glob.sync('packages/*/src').map(srcPath => {
     const packageName = path.basename(path.dirname(srcPath));
@@ -10,7 +11,25 @@ const aliases = glob.sync('packages/*/src').map(srcPath => {
     };
 });
 
+const custom = [
+    'container',
+    'image',
+    'sprite',
+    'shader',
+    'text',
+    'comment',
+    'custom',
+    'container-custom'
+];
+
 export default defineConfig({
+    plugins: [
+        jsx({
+            isCustomElement: tag => {
+                return custom.includes(tag) || tag.startsWith('g-');
+            }
+        })
+    ],
     resolve: {
         alias: [...aliases]
     }
